3c04a1f19334117f8ad02d60237fa57545021a09
Wayne-Thornberry <way-net@outlook.com>
2022-10-14 16:03:06 +0100

    fixes for paths

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,6 @@
 ﻿Remove-Item "D:\ProjectOnline\resources\client-core\*"  -Recurse
-Copy-Item -Path "E:\OneDrive\Repo\Project Online\data\resources\ProlineCore\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
-Copy-Item -Path "E:\OneDrive\Repo\RPOnline\data\components\ProlineCore\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
-Copy-Item -Path "E:\OneDrive\Repo\RPOnline\data\core\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
+Copy-Item -Path "C:\Users\Wayno717\OneDrive\Repo\RPOnline\data\components\ProlineCore\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
+Copy-Item -Path "C:\Users\Wayno717\OneDrive\Repo\RPOnline\data\core\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
 Copy-Item -Path "E:\artifacts\ProlineCore\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
 Copy-Item -Path "E:\artifacts\RPOnline\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
 Remove-Item "D:\ProjectOnline\resources\client-core\CitizenFX.Core.*.dll" -Recurse
