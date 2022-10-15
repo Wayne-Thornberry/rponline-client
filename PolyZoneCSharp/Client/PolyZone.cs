4f1faed225e8266a593c03e2f9902577ed97409f
Wayne-Thornberry <way-net@outlook.com>
2022-10-15 13:34:42 +0100

    introduction  to polyzone csharp

---
+++ PolyZoneCSharp/Client/PolyZone.cs
@@ -0,0 +1,51 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyZoneCSharp
+{
+    internal class PolyZone : PolyZoneShared
+    {
+        static float? minZ = null;
+        static float? maxZ = null;
+        private static object createdZone;
+
+        public float handleInput(float center)
+        {
+            var rot = API.GetGameplayCamRot(2);
+            center = handleArrowInput(center, rot.Z);
+            return center;
+        }
+
+        public static void polyStart(string name) {
+            var coords = API.GetEntityCoords(API.PlayerPedId(), true);
+            createdZone = PolyZoneShared.Create(new Vector2(coords.X, coords.Y), new { name = name, useGrid = false});
+            Task.Factory.StartNew(() =>
+            {
+                while (createdZone != null)
+                {
+                    //Have to convert the point to a vector3 prior to calling handleInput,
+                    //then convert it back to vector2 afterwards
+                    var lastPoint = createdZone.points[createdZone.points.Length - 1];
+                    lastPoint = new Vector3(lastPoint.x, lastPoint.y, 0.0);
+                    lastPoint = handleInput(lastPoint);
+                    createdZone.points[createdZone.points.Length - 1] = new Vector2(lastPoint.x, lastPoint.y);
+                    BaseScript.Delay(0);
+                }
+            });
+
+            minZ = coords.Z; 
+            maxZ= coords.Z;
+        }
+        
+
+        public static void polyFinish()
+        {
+            BaseScript.TriggerServerEvent("polyzone:printPoly", new { name = createdZone.name, points = createdZone.points, minZ = minZ, maxZ = maxZ });
+        }
+    }
+}
---
+++ PolyZoneCSharp/PolyZoneShared.cs
@@ -0,0 +1,119 @@
+﻿using CitizenFX.Core;
+
+namespace PolyZoneCSharp
+{
+    internal class PolyOptions
+    {
+        public string name;
+        public Vector2[] points;
+        public Vector3 center;
+        public float size;
+        public float max;
+        public float min;
+        public float area;
+        public float? minZ;
+        public float? maxZ;
+        public bool useGrid;
+        public bool lazyGrid;
+        public int gridDivisions;
+        public object debugColors;
+        public bool debugPoly;
+        public bool debugGrid;
+        public object data;
+        public bool isPolyZone;
+    }
+
+    internal class Poly
+    {
+       public string name;
+       public Vector2[] points;
+       public Vector3 center;
+       public float size;
+       public float max;
+       public float min;
+       public float area;
+       public float? minZ;
+       public float? maxZ;
+       public bool useGrid;
+       public bool lazyGrid;
+       public int gridDivisions;
+       public object debugColors;
+       public bool debugPoly;
+       public bool debugGrid;
+       public object data;
+       public bool isPolyZone;
+    }
+
+    internal class PolyZoneShared
+    {
+        public static Poly New(Vector2[] points, PolyOptions options)
+        {
+            if (points == null)
+            {
+                //print("[PolyZone] Error: Passed nil points table to PolyZone:Create() {name="..options.name.."}");
+                return null;
+            }
+            if (points.Length < 3)
+            {
+               // print("[PolyZone] Warning: Passed points table with less than 3 points to PolyZone:Create() {name="..options.name.."}");
+            }
+
+            options = options != null ? options : new PolyOptions();
+            var useGrid = options.useGrid;
+            if (useGrid == null) { useGrid = true; }
+            var lazyGrid = options.lazyGrid;
+            if (lazyGrid == null) { lazyGrid = true; }
+            var poly = new Poly()
+            {
+                name = string.IsNullOrEmpty(options.name) ? options.name : null,
+                points = points,
+                center = options.center,
+                size = options.size,
+                max = options.max,
+                min = options.min,
+                area = options.area,
+                minZ = options.minZ != null ? options.minZ : null,
+                maxZ = options.maxZ != null ? options.maxZ : null,
+                useGrid = useGrid,
+                lazyGrid = lazyGrid,
+                gridDivisions = options.gridDivisions != null ? options.gridDivisions : 30,
+                debugColors = options.debugColors != null ? options.debugColors : new { },
+                debugPoly = options.debugPoly != null ? options.debugPoly : false,
+                debugGrid = options.debugGrid != null ? options.debugGrid : false,
+                data = options.data != null ? options.data : new { },
+                isPolyZone = true,
+            };
+            if (poly.debugGrid) { poly.lazyGrid = false; }
+            _calculatePoly(poly, options);
+            setmetatable(poly, self);
+            this.__index = self;
+            return poly;
+        }
+
+        public static object Create(object points, object options)
+        {
+            var poly = New(points, options);
+            _initDebug(poly, options);
+            return poly;
+        }
+
+        public static object isPointInside(object point)
+        {
+            if (self.destroyed)
+            {
+                print("[PolyZone] Warning: Called isPointInside on destroyed zone {name="..self.name.."}");
+                return false;
+            }
+            return _pointInPoly(point, self)
+        }
+
+        public static object destroy()
+        {
+            self.destroyed = true;
+            if (self.debugPoly || self.debugGrid)
+            {
+                print("[PolyZone] Debug: Destroying zone {name="..self.name.."}");
+            }
+        }
+    }
+}
