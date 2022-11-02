9ca7ea6b03cddcfce12d00c44ac58118a612e3a0
Wayne-Thornberry <way-net@outlook.com>
2022-11-02 18:56:02 +0000

    updated some configs

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,5 @@
 ﻿Remove-Item "D:\ProjectOnline\resources\client-core\*"  -Recurse
-Copy-Item -Path ".\data\core\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse 
-Copy-Item -Path ".\data\components\ProlineCore\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
+Copy-Item -Path ".\data\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse 
 Copy-Item -Path ".\vendor\ProlineCore\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
 Copy-Item -Path ".\artifacts\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
 Remove-Item "D:\ProjectOnline\resources\client-core\CitizenFX.Core.*.dll" -Recurse
