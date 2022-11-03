8bf659559c34fac369212994a4111ec7206fd193
Wayne-Thornberry <way-net@outlook.com>
2022-11-03 18:16:44 +0000

    removed manifest, updated deploy

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,7 @@
 ﻿Remove-Item "D:\ProjectOnline\resources\client-core\*"  -Recurse
-Copy-Item -Path ".\data\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse 
 Copy-Item -Path ".\vendor\ProlineCore\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
+Copy-Item -Path ".\vendor\OnlineEngine\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
+Copy-Item -Path ".\data\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse 
 Copy-Item -Path ".\artifacts\*" -Destination "D:\ProjectOnline\resources\client-core\" -Recurse
 Remove-Item "D:\ProjectOnline\resources\client-core\CitizenFX.Core.*.dll" -Recurse
 Remove-Item "D:\ProjectOnline\resources\client-core\*.pdb" -Recurse
